bf38b310d81f77cc55d013f5aef0b9b0077bb3d9
ackermanr <ackerman.ro@gmail.com>
2014-06-23 10:43:44 -0400

    Updated the styling and the logic for the login function.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,16 @@
 class SessionsController < ApplicationController
   def new
+    session[:return_to] = request.referer
+    if session[:return_to] == request.original_url
+      redirect_to :controller => "articles", :action => "index"
+    end 
   end
   
   def create
     user = User.authenticate(params[:email], params[:password])
     if user
       session[:user_id] = user.id
-      redirect_to root_url, :notice => "Logged in!"
+      redirect_to session[:return_to] 
     else
       flash.now.alert = "Invalid email or password"
       render 'new'
@@ -15,6 +19,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, :notice => "Logged out"
+    session[:return_to] = request.referer
+    redirect_to session[:return_to]
   end
 end
