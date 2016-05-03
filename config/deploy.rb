51f39611f900e4355e2b9f4a94e7bfb693920a75
RC Ackerman <ackerman.ro@gmail.com>
2016-05-02 22:04:37 -0400

    Quick test of a theory

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :user,            'rc'
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/rcackerman.com/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_bind,       "unix:///#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
